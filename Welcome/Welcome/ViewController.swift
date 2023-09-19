b699e179349b7167234f09b48bb117542f3ac629
mcazarjr <meraldocazar@gmail.com>
2023-09-18 20:24:30 -0700

    feat: Finish implementation of third assignment

---
+++ Welcome/Welcome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Welcome
+//
+//  Created by Meraldo Cazar Jr. on 2023-09-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
