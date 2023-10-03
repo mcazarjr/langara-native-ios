d72082a5fec2e4de848f53c25e57cdb64ca6090c
mcazarjr <meraldocazar@gmail.com>
2023-10-03 00:04:39 -0700

    chore: initial commit for night porter assignment

---
+++ Night Porter/Night Porter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Night Porter
+//
+//  Created by Meraldo Cazar Jr. on 2023-10-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
