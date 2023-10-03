85d39fff1b54d8bc43a71be88b58be1584042d57
mcazarjr <meraldocazar@gmail.com>
2023-10-02 23:34:13 -0700

    chore: initial commit for table view demo assignment

---
+++ Table View Demo/Table View Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Table View Demo
+//
+//  Created by Meraldo Cazar Jr. on 2023-10-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
