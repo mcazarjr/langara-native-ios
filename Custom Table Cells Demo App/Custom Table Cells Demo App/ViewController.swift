d1070394107b8c5d3a1ba803444c45ed5b852d87
mcazarjr <meraldocazar@gmail.com>
2023-10-16 15:33:07 -0700

    chore: finalize custome table cells assignment

--- Custom Table Cells Demo App/Custom Table Cells Demo App/ViewController.swift
+++ Custom Table Cells Demo App/Custom Table Cells Demo App/ViewController.swift
@@ -7,7 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("Calling cellForRowAtIndexPath for row: \(indexPath.row)")
+        
+        // Instantiate new Table View Cell
+        let myCell = tableView.dequeueReusableCell(withIdentifier: "normalCell", for: indexPath)
+        // Set the Contents
+        myCell.textLabel?.text = "This is row number \(indexPath.row)"
+        myCell.imageView?.image = UIImage(named: "clock")
+        
+        myCell.accessoryType = .disclosureIndicator
+        myCell.detailTextLabel?.text = "This is some detail text"
+        return myCell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
