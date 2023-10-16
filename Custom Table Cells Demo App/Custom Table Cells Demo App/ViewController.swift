6a75d4298795edad18888aac3aa1d01f550d5420
mcazarjr <meraldocazar@gmail.com>
2023-10-16 14:59:44 -0700

    chore: initialize Custom Table Cells Demo App Assignment

---
+++ Custom Table Cells Demo App/Custom Table Cells Demo App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Custom Table Cells Demo App
+//
+//  Created by Meraldo Cazar Jr. on 2023-10-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
